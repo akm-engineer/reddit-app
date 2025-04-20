24a9e0404b5d3f0ecd4b6e2c242d5d3aedfcd4fb
Akshay Yelle <02b3akshay@gmail.com>
2025-04-20 16:24:23 +0530

    add final sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,18 +1,22 @@
 import type { PropsWithChildren } from "react"
-import { Sidebar } from "./_components/sidebar"
+import { BottomBar } from "./_components/bottom-bar"
 import { Navbar } from "./_components/navbar"
+import { AppSidebar } from "./_components/app-sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen h-full flex-col">
-      <div className="max-w-dvw mx-auto p-4 sm:pl-12 sm:pr-2 sm:py-6 flex w-full grow gap-5">
-        <Sidebar className="sticky hidden sm:block flex-none space-y-3 rounded-md px-6 py-4 shadow-sm xl:w-72" />
-        <div className="w-full space-y-4 mx-0 sm:mx-4 lg:mx-8">
-          <Navbar />
-          <div className="mx-0 sm:mx-2">{children}</div>
+    <div className="flex min-h-screen w-full h-full flex-col">
+      <SidebarProvider>
+        <div className="max-w-dvw mx-auto p-4 sm:pl-12 sm:pr-2 sm:py-6 flex w-full grow gap-5">
+          <AppSidebar />
+          <div className="w-full space-y-4 mx-0 sm:mx-4 lg:mx-8">
+            <Navbar />
+            <div className="mx-0 sm:mx-2">{children}</div>
+          </div>
         </div>
-      </div>
-      <Sidebar className="sticky bottom-0 flex w-full justify-center gap-8 border-t p-3 sm:hidden" />
+      </SidebarProvider>
+      <BottomBar className="sticky bottom-0 flex w-full justify-center gap-8 border-t p-3 sm:hidden" />
     </div>
   )
 }
